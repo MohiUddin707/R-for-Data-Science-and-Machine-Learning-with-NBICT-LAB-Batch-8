6feb3005aa5d92845b5eda994c6fed4e50f69a6d
MohiUddin707 <mohiuddinae018033@gmail.com>
2025-04-30 22:50:52 +0600

    basic code for learning

---
+++ RDSML-Day-3/r_basics_part1.R
@@ -0,0 +1,8 @@
+#calculating some health parameter
+my_age = 97
+my_weight <- 60
+my_height = 65
+x = (my_weight*my_age)/my_height
+print(x)
+students_height = c(12, 14, 20, 30)
+mean(students_height)
