869c32877253477437750eb152633273dc103939
MohiUddin707 <mohiuddinae018033@gmail.com>
2025-05-07 22:50:51 +0600

    day 4 practice

---
+++ RDSML-Day-4  R Programming Basics (Part-2)/R_basics_part_2.R
@@ -0,0 +1,19 @@
+a = 11
+b = 7.19
+class(a)
+name = "mohi uddin"
+class(name)
+print(name)
+a > b
+class (a>b)
+x = 19
+y= 22-3
+print(x == y)
+print (x != y)
+print( y>=x)
+4==4 && 5==5
+x != y || x == y
+z=6+9-8*2/3
+print(z)
+11/5
+11%/%5
