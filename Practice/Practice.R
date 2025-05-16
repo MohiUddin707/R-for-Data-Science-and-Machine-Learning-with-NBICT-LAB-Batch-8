bd04c8184899f200cd1b45d4f541f032f055705d
MohiUddin707 <mohiuddinae018033@gmail.com>
2025-05-16 23:22:30 +0600

    practice

---
+++ Practice/Practice.R
@@ -0,0 +1,48 @@
+d = c(TRUE,FALSE)
+x = as.numeric(d)
+x
+subo = c(1,2,3,4,5,6,7,8,9)
+summary(subo)
+mean(subo)
+as.logical(subo)
+as.character(subo)
+sew = c(1:100)
+sew
+sew = c(1:50, 60, 80,90)
+sew
+sew[5]
+sew[52]
+seq = c(seq(1,10))
+seq
+seq = c(seq(1,10,by=3))
+seq
+sew
+mean(sew)
+median(sew)
+deviance(sew)
+class(sew)
+is.numeric(sew)
+is.logical(sew)
+names = c( "X", "Y" , "Z", "M", "N", "J")
+summary(names)
+names = factor(names)
+names
+names = factor(ordered = FALSE  vels = c("J","M","N","Y","Z"))
+names
+summary(names)
+names[5]
+number = c(1:5)
+names(number) =c("b","c","d","f","g")
+number
+number["b"]
+number
+x = c("B":"Z")
+x
+#list in R
+a = c(2,4,5,6)
+b =c("x","d","q")
+c = "money!"
+m = list(a,b,c)
+m
+m[1]
+
