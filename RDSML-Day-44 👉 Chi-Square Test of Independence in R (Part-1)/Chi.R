5703459a6c7e1de61159844a56b7cf9f5e4d58e2
MohiUddin707 <mohiuddinae018033@gmail.com>
2025-10-06 22:33:38 +0600

    Chi square

---
+++ RDSML-Day-44 👉 Chi-Square Test of Independence in R (Part-1)/Chi.R
@@ -0,0 +1,31 @@
+#importing the dataset
+housetasks = read.delim("housetasks.txt", row.names =1)
+
+#contigency table can be visualized using the function 
+#ballonplot(). This function draws a graphical matrix where
+# each cell contains a dot whose size reflects the relative
+#magnitude of the corresponding component
+
+install.packages("gplots")
+library("gplots")
+as.matrix(housetasks)
+dt = as.table(as.matrix(housetasks))
+dt
+balloonplot(t(dt), main = "Housetasks Visualization",
+            label = F, show.margins = F, 
+            xlab = "", ylab = "")
+#It is also possible to visualized a contigency table
+#as a mosaic plot. This can be done using the function 
+# mosaicplot() from the built in R packages graphics
+
+library("graphics")
+mosaicplot(dt, shade = T,las=2, main ="Housetasks")
+# Blue colors indicate that the observed value is
+#higher than the expected values. Red color indicates
+# that the observed value is lower than the expected value
+
+
+
+
+
+
